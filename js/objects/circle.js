41291c11365cd59e5e6d87d7c19b57930f14b6e0
Nathan Pham <nathanpham.me@gmail.com>
2021-08-14 19:46:53 -0700

    cool map grid

--- js/objects/circle.js
+++ js/objects/circle.js
@@ -1,12 +1,14 @@
 export const circle = ({x=0, y=0, radius=100}) => {
     return ({ctx}) => {
-        // ctx.fillStyle = "#000"
         
         ctx.beginPath()
         ctx.arc(x, y, radius, 0, 2 * Math.PI)
 
-        ctx.strokeStyle = "#000"
-        ctx.lineWidth = 25
-        ctx.stroke()
+        ctx.fillStyle = "#000"
+        ctx.fill()
+
+        // ctx.strokeStyle = "#000"
+        // ctx.lineWidth = 15
+        // ctx.stroke()
     }
 }
--- js/objects/grid.js
+++ js/objects/grid.js
@@ -1,12 +1,9 @@
 import {circle} from "./circle.js"
 import {lerp} from "../lib/math.js"
 
-export const grid = () => {
-    const margin = 250
+const create = (size) => {
     const points = []
-    const size = 5
 
-    // create uv grid
     for(let x = 0; x < size; x++) {
         for(let y = 0; y < size; y++) {
             const u = x / (size - 1)
@@ -15,6 +12,17 @@ export const grid = () => {
         }
     }
 
+    return points
+}
+
+
+export const grid = () => {
+    const margin = 200
+    const size = 50
+
+    // create uv grid
+    const points = create(size).filter(() => Math.random() > 0.5)
+
     return ({ctx, dimensions: [width, height]}) => {
         for(const point of points) {
             const [u, v] = point
@@ -22,7 +30,7 @@ export const grid = () => {
             const x = lerp(margin, width - margin, u)
             const y = lerp(margin, height - margin, v)
             
-            const renderCircle = circle({x, y, radius: 75})
+            const renderCircle = circle({x, y, radius: 10})
             renderCircle({ctx})
         }
     }
